2997e34823de4c25f84938329b3f7cdd8341acad
GustavoLyra23 <gustavolyra23@gmail.com>
2024-11-03 04:22:57 -0300

    fixed issue where the piece didn't go to the correct place

--- app.js
+++ app.js
@@ -1,25 +1,62 @@
 const board = document.querySelector(".chessboard");
 
 const initialBoard = [
-  ["./icons/br.png", "./icons/bn.png", "./icons/bb.png", "./icons/bq.png", "./icons/bk.png", "./icons/bb.png", "./icons/bn.png", "./icons/br.png"],
-  ["./icons/bp.png", "./icons/bp.png", "./icons/bp.png", "./icons/bp.png", "./icons/bp.png", "./icons/bp.png", "./icons/bp.png", "./icons/bp.png"],
+  [
+    "./icons/br.png",
+    "./icons/bn.png",
+    "./icons/bb.png",
+    "./icons/bq.png",
+    "./icons/bk.png",
+    "./icons/bb.png",
+    "./icons/bn.png",
+    "./icons/br.png",
+  ],
+  [
+    "./icons/bp.png",
+    "./icons/bp.png",
+    "./icons/bp.png",
+    "./icons/bp.png",
+    "./icons/bp.png",
+    "./icons/bp.png",
+    "./icons/bp.png",
+    "./icons/bp.png",
+  ],
   ["", "", "", "", "", "", "", ""],
   ["", "", "", "", "", "", "", ""],
   ["", "", "", "", "", "", "", ""],
   ["", "", "", "", "", "", "", ""],
-  ["./icons/wp.png", "./icons/wp.png", "./icons/wp.png", "./icons/wp.png", "./icons/wp.png", "./icons/wp.png", "./icons/wp.png", "./icons/wp.png"],
-  ["./icons/wr.png", "./icons/wn.png", "./icons/wb.png", "./icons/wq.png", "./icons/wk.png", "./icons/wb.png", "./icons/wn.png", "./icons/wr.png"],
+  [
+    "./icons/wp.png",
+    "./icons/wp.png",
+    "./icons/wp.png",
+    "./icons/wp.png",
+    "./icons/wp.png",
+    "./icons/wp.png",
+    "./icons/wp.png",
+    "./icons/wp.png",
+  ],
+  [
+    "./icons/wr.png",
+    "./icons/wn.png",
+    "./icons/wb.png",
+    "./icons/wq.png",
+    "./icons/wk.png",
+    "./icons/wb.png",
+    "./icons/wn.png",
+    "./icons/wr.png",
+  ],
 ];
 
-const copyBoard = initialBoard.map((row) => [...row]);
+let copyBoard = initialBoard.map((row) => [...row]);
 let currentPlayer = "white";
+let selectedPiece = null;
 
 function createBoard() {
-  board.innerHTML = ""; 
+  board.innerHTML = "";
   for (let i = 0; i < 8; i++) {
     for (let j = 0; j < 8; j++) {
       const square = createSquare(i, j);
-      const piece = initialBoard[i][j];
+      const piece = copyBoard[i][j];
       if (piece) {
         const pieceElement = createPieceElement(piece, i, j);
         square.appendChild(pieceElement);
@@ -48,27 +85,26 @@ function createPieceElement(piece, row, col) {
   pieceElement.classList.add("chess-piece");
   pieceElement.dataset.row = row;
   pieceElement.dataset.col = col;
+  pieceElement.addEventListener("click", pieceClickHandler);
   return pieceElement;
 }
 
 document.addEventListener("keypress", () => {
   document.querySelector("h2").innerText = "Game started!";
-  document.querySelectorAll(".chess-piece").forEach((piece) => {
-    piece.addEventListener("click", pieceClickHandler);
-  });
 });
 
 function pieceClickHandler(e) {
   const piece = e.target;
+  const pieceRow = parseInt(piece.dataset.row);
+  const pieceCol = parseInt(piece.dataset.col);
   const pieceColor = piece.src.includes("w") ? "white" : "black";
   if (pieceColor !== currentPlayer) return;
 
   clearHighlights();
+
+  selectedPiece = piece;
   const moves = checkPieceToMove(piece);
   highlightMoves(moves);
-  console.log(moves);
-  console.log(e.target.classList);
-  console.log("You clicked on a chess piece!");
 
   document.querySelectorAll(".highlight").forEach((square) => {
     square.addEventListener("click", squareClickHandler);
@@ -76,210 +112,78 @@ function pieceClickHandler(e) {
 }
 
 function squareClickHandler(e) {
-  const piece = document.querySelector(".chess-piece[data-selected='true']");
-  if (piece) {
-    const targetSquare = e.target.closest(".square");
-    if (targetSquare && targetSquare.classList.contains("highlight")) {
-      targetSquare.appendChild(piece);
-      const row = parseInt(piece.dataset.row);
-      const col = parseInt(piece.dataset.col);
-      const newRow = parseInt(targetSquare.dataset.row);
-      const newCol = parseInt(targetSquare.dataset.col);
-      copyBoard[newRow][newCol] = copyBoard[row][col];
-      copyBoard[row][col] = "";
-      piece.dataset.row = newRow;
-      piece.dataset.col = newCol;
-      piece.removeAttribute("data-selected");
-      clearHighlights();
-      document.querySelectorAll(".highlight").forEach((square) => {
-        square.removeEventListener("click", squareClickHandler);
-      });
-      currentPlayer = currentPlayer === "white" ? "black" : "white";
-      checkGameStatus();
+  if (!selectedPiece) return;
+
+  const targetSquare = e.target.closest(".square");
+  if (targetSquare && targetSquare.classList.contains("highlight")) {
+    const targetRow = parseInt(targetSquare.dataset.row);
+    const targetCol = parseInt(targetSquare.dataset.col);
+    const sourceRow = parseInt(selectedPiece.dataset.row);
+    const sourceCol = parseInt(selectedPiece.dataset.col);
+    copyBoard[targetRow][targetCol] = copyBoard[sourceRow][sourceCol];
+    copyBoard[sourceRow][sourceCol] = "";
+    const targetPiece = targetSquare.querySelector(".chess-piece");
+    if (targetPiece && targetPiece !== selectedPiece) {
+      targetPiece.remove();
     }
+
+    selectedPiece.dataset.row = targetRow;
+    selectedPiece.dataset.col = targetCol;
+    targetSquare.appendChild(selectedPiece);
+
+    selectedPiece = null;
+    clearHighlights();
+
+    document.querySelectorAll(".highlight").forEach((square) => {
+      square.removeEventListener("click", squareClickHandler);
+    });
+
+    currentPlayer = currentPlayer === "white" ? "black" : "white";
+
+    checkGameStatus();
   }
 }
 
-function calculatePawnMoves(row, column) {
-  const moves = initializeMovesMatrix();
-  const piece = copyBoard[row][column];
+function checkPieceToMove(piece) {
+  const row = parseInt(piece.dataset.row);
+  const col = parseInt(piece.dataset.col);
+  const pieceSrc = piece.src.split("/").pop();
 
-  if (piece === "./icons/bp.png") {
-    if (row + 1 < 8 && copyBoard[row + 1][column] === "") {
-      moves[row + 1][column] = true;
-      if (row === 1 && copyBoard[row + 2][column] === "") {
-        moves[row + 2][column] = true;
-      }
-    }
-    if (row + 1 < 8 && column + 1 < 8 && copyBoard[row + 1][column + 1].startsWith("./icons/w")) {
-      moves[row + 1][column + 1] = true;
-    }
-    if (row + 1 < 8 && column - 1 >= 0 && copyBoard[row + 1][column - 1].startsWith("./icons/w")) {
-      moves[row + 1][column - 1] = true;
-    }
-  } else if (piece === "./icons/wp.png") {
-    if (row - 1 >= 0 && copyBoard[row - 1][column] === "") {
-      moves[row - 1][column] = true;
-      if (row === 6 && copyBoard[row - 2][column] === "") {
-        moves[row - 2][column] = true;
-      }
-    }
-    if (row - 1 >= 0 && column + 1 < 8 && copyBoard[row - 1][column + 1].startsWith("./icons/b")) {
-      moves[row - 1][column + 1] = true;
-    }
-    if (row - 1 >= 0 && column - 1 >= 0 && copyBoard[row - 1][column - 1].startsWith("./icons/b")) {
-      moves[row - 1][column - 1] = true;
-    }
+  switch (pieceSrc) {
+    case "wp.png":
+    case "bp.png":
+      return calculatePawnMoves(row, col);
+    case "wr.png":
+    case "br.png":
+      return calculateRookMoves(row, col);
+    case "wn.png":
+    case "bn.png":
+      return calculateKnightMoves(row, col);
+    case "wb.png":
+    case "bb.png":
+      return calculateBishopMoves(row, col);
+    case "wq.png":
+    case "bq.png":
+      return calculateQueenMoves(row, col);
+    case "wk.png":
+    case "bk.png":
+      return calculateKingMoves(row, col);
+    default:
+      return initializeMovesMatrix();
   }
-
-  return moves;
-}
-
-function calculateRookMoves(row, column) {
-  const moves = initializeMovesMatrix();
-  const piece = copyBoard[row][column];
-  const directions = [
-    [1, 0], [-1, 0], [0, 1], [0, -1]
-  ];
-
-  directions.forEach(([dx, dy]) => {
-    let x = row + dx;
-    let y = column + dy;
-    while (x >= 0 && x < 8 && y >= 0 && y < 8) {
-      if (copyBoard[x][y] === "") {
-        moves[x][y] = true;
-      } else {
-        if (piece.startsWith("./icons/w") && copyBoard[x][y].startsWith("./icons/b")) {
-          moves[x][y] = true;
-        } else if (piece.startsWith("./icons/b") && copyBoard[x][y].startsWith("./icons/w")) {
-          moves[x][y] = true;
-        }
-        break;
-      }
-      x += dx;
-      y += dy;
-    }
-  });
-
-  return moves;
-}
-
-function calculateKnightMoves(row, column) {
-  const moves = initializeMovesMatrix();
-  const piece = copyBoard[row][column];
-  const knightMoves = [
-    [2, 1], [2, -1], [-2, 1], [-2, -1],
-    [1, 2], [1, -2], [-1, 2], [-1, -2]
-  ];
-
-  knightMoves.forEach(([dx, dy]) => {
-    const x = row + dx;
-    const y = column + dy;
-    if (x >= 0 && x < 8 && y >= 0 && y < 8) {
-      if (copyBoard[x][y] === "" || (piece.startsWith("./icons/w") && copyBoard[x][y].startsWith("./icons/b")) || (piece.startsWith("./icons/b") && copyBoard[x][y].startsWith("./icons/w"))) {
-        moves[x][y] = true;
-      }
-    }
-  });
-
-  return moves;
-}
-
-function calculateBishopMoves(row, column) {
-  const moves = initializeMovesMatrix();
-  const piece = copyBoard[row][column];
-  const directions = [
-    [1, 1], [1, -1], [-1, 1], [-1, -1]
-  ];
-
-  directions.forEach(([dx, dy]) => {
-    let x = row + dx;
-    let y = column + dy;
-    while (x >= 0 && x < 8 && y >= 0 && y < 8) {
-      if (copyBoard[x][y] === "") {
-        moves[x][y] = true;
-      } else {
-        if (piece.startsWith("./icons/w") && copyBoard[x][y].startsWith("./icons/b")) {
-          moves[x][y] = true;
-        } else if (piece.startsWith("./icons/b") && copyBoard[x][y].startsWith("./icons/w")) {
-          moves[x][y] = true;
-        }
-        break;
-      }
-      x += dx;
-      y += dy;
-    }
-  });
-
-  return moves;
-}
-
-function calculateQueenMoves(row, column) {
-  const rookMoves = calculateRookMoves(row, column);
-  const bishopMoves = calculateBishopMoves(row, column);
-  return rookMoves.map((row, i) => row.map((cell, j) => cell || bishopMoves[i][j]));
-}
-
-function calculateKingMoves(row, column) {
-  const moves = initializeMovesMatrix();
-  const piece = copyBoard[row][column];
-  const kingMoves = [
-    [1, 0], [-1, 0], [0, 1], [0, -1],
-    [1, 1], [1, -1], [-1, 1], [-1, -1]
-  ];
-
-  kingMoves.forEach(([dx, dy]) => {
-    const x = row + dx;
-    const y = column + dy;
-    if (x >= 0 && x < 8 && y >= 0 && y < 8) {
-      if (copyBoard[x][y] === "" || (piece.startsWith("./icons/w") && copyBoard[x][y].startsWith("./icons/b")) || (piece.startsWith("./icons/b") && copyBoard[x][y].startsWith("./icons/w"))) {
-        moves[x][y] = true;
-      }
-    }
-  });
-
-  return moves;
 }
 
 function initializeMovesMatrix() {
   return Array.from({ length: 8 }, () => Array(8).fill(false));
 }
 
-function checkPieceToMove(piece) {
-  const row = parseInt(piece.dataset.row);
-  const column = parseInt(piece.dataset.col);
-  const pieceSrc = piece.src.split("/").pop();
-  console.log(pieceSrc);
-  piece.setAttribute("data-selected", "true");
-  switch (pieceSrc) {
-    case "bp.png":
-    case "wp.png":
-      return calculatePawnMoves(row, column);
-    case "br.png":
-    case "wr.png":
-      return calculateRookMoves(row, column);
-    case "bn.png":
-    case "wn.png":
-      return calculateKnightMoves(row, column);
-    case "bb.png":
-    case "wb.png":
-      return calculateBishopMoves(row, column);
-    case "bq.png":
-    case "wq.png":
-      return calculateQueenMoves(row, column);
-    case "bk.png":
-    case "wk.png":
-      return calculateKingMoves(row, column);
-    default:
-      return initializeMovesMatrix();
-  }
-}
-
 function highlightMoves(moves) {
-  for (let i = 0; i < 8; i++) {
-    for (let j = 0; j < 8; j++) {
+  for (let i = 0; i < moves.length; i++) {
+    for (let j = 0; j < moves[i].length; j++) {
       if (moves[i][j]) {
-        const square = document.querySelector(`.square[data-row='${i}'][data-col='${j}']`);
+        const square = document.querySelector(
+          `.square[data-row='${i}'][data-col='${j}']`
+        );
         square.classList.add("highlight");
       }
     }
@@ -289,27 +193,185 @@ function highlightMoves(moves) {
 function clearHighlights() {
   document.querySelectorAll(".highlight").forEach((square) => {
     square.classList.remove("highlight");
+    square.removeEventListener("click", squareClickHandler);
   });
 }
 
-function checkGameStatus() {
-  // Basic checkmate or tie checker
-  const whiteKing = document.querySelector("img[src='./icons/wk.png']");
-  const blackKing = document.querySelector("img[src='./icons/bk.png']");
-  if (!whiteKing) {
-    alert("Black wins!");
-  } else if (!blackKing) {
-    alert("White wins!");
-  } else if (isTie()) {
-    alert("It's a tie!");
+function calculatePawnMoves(row, col) {
+  const moves = initializeMovesMatrix();
+  const piece = copyBoard[row][col];
+  const direction = piece.includes("/w") ? -1 : 1;
+  const startRow = piece.includes("/w") ? 6 : 1;
+
+  if (copyBoard[row + direction][col] === "") {
+    moves[row + direction][col] = true;
+    if (row === startRow && copyBoard[row + 2 * direction][col] === "") {
+      moves[row + 2 * direction][col] = true;
+    }
   }
+  for (let dx of [-1, 1]) {
+    const newRow = row + direction;
+    const newCol = col + dx;
+    if (newCol >= 0 && newCol < 8) {
+      const targetPiece = copyBoard[newRow] && copyBoard[newRow][newCol];
+      if (
+        targetPiece &&
+        targetPiece.includes(piece.includes("/w") ? "/b" : "/w")
+      ) {
+        moves[newRow][newCol] = true;
+      }
+    }
+  }
+
+  return moves;
 }
 
-function isTie() {
-  // Basic tie checker (no legal moves for both players)
-  const whitePieces = document.querySelectorAll("img[src*='./icons/w']");
-  const blackPieces = document.querySelectorAll("img[src*='./icons/b']");
-  return whitePieces.length === 1 && blackPieces.length === 1;
+function calculateRookMoves(row, col) {
+  const moves = initializeMovesMatrix();
+  const piece = copyBoard[row][col];
+  const directions = [
+    { dr: -1, dc: 0 },
+    { dr: 1, dc: 0 },
+    { dr: 0, dc: -1 },
+    { dr: 0, dc: 1 },
+  ];
+
+  for (const { dr, dc } of directions) {
+    let r = row + dr;
+    let c = col + dc;
+    while (r >= 0 && r < 8 && c >= 0 && c < 8) {
+      const targetPiece = copyBoard[r][c];
+      if (targetPiece === "") {
+        moves[r][c] = true;
+      } else {
+        if (targetPiece.includes(piece.includes("/w") ? "/b" : "/w")) {
+          moves[r][c] = true;
+        }
+        break;
+      }
+      r += dr;
+      c += dc;
+    }
+  }
+
+  return moves;
+}
+
+function calculateKnightMoves(row, col) {
+  const moves = initializeMovesMatrix();
+  const piece = copyBoard[row][col];
+  const knightMoves = [
+    { dr: -2, dc: -1 },
+    { dr: -2, dc: 1 },
+    { dr: -1, dc: -2 },
+    { dr: -1, dc: 2 },
+    { dr: 1, dc: -2 },
+    { dr: 1, dc: 2 },
+    { dr: 2, dc: -1 },
+    { dr: 2, dc: 1 },
+  ];
+
+  for (const { dr, dc } of knightMoves) {
+    const r = row + dr;
+    const c = col + dc;
+    if (r >= 0 && r < 8 && c >= 0 && c < 8) {
+      const targetPiece = copyBoard[r][c];
+      if (
+        targetPiece === "" ||
+        targetPiece.includes(piece.includes("/w") ? "/b" : "/w")
+      ) {
+        moves[r][c] = true;
+      }
+    }
+  }
+
+  return moves;
+}
+
+function calculateBishopMoves(row, col) {
+  const moves = initializeMovesMatrix();
+  const piece = copyBoard[row][col];
+  const directions = [
+    { dr: -1, dc: -1 },
+    { dr: -1, dc: 1 },
+    { dr: 1, dc: -1 },
+    { dr: 1, dc: 1 },
+  ];
+
+  for (const { dr, dc } of directions) {
+    let r = row + dr;
+    let c = col + dc;
+    while (r >= 0 && r < 8 && c >= 0 && c < 8) {
+      const targetPiece = copyBoard[r][c];
+      if (targetPiece === "") {
+        moves[r][c] = true;
+      } else {
+        if (targetPiece.includes(piece.includes("/w") ? "/b" : "/w")) {
+          moves[r][c] = true;
+        }
+        break;
+      }
+      r += dr;
+      c += dc;
+    }
+  }
+
+  return moves;
+}
+
+function calculateQueenMoves(row, col) {
+  const rookMoves = calculateRookMoves(row, col);
+  const bishopMoves = calculateBishopMoves(row, col);
+  const moves = initializeMovesMatrix();
+
+  for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+      moves[i][j] = rookMoves[i][j] || bishopMoves[i][j];
+    }
+  }
+
+  return moves;
+}
+
+function calculateKingMoves(row, col) {
+  const moves = initializeMovesMatrix();
+  const piece = copyBoard[row][col];
+  const kingMoves = [
+    { dr: -1, dc: -1 },
+    { dr: -1, dc: 0 },
+    { dr: -1, dc: 1 },
+    { dr: 0, dc: -1 },
+    { dr: 0, dc: 1 },
+    { dr: 1, dc: -1 },
+    { dr: 1, dc: 0 },
+    { dr: 1, dc: 1 },
+  ];
+
+  for (const { dr, dc } of kingMoves) {
+    const r = row + dr;
+    const c = col + dc;
+    if (r >= 0 && r < 8 && c >= 0 && c < 8) {
+      const targetPiece = copyBoard[r][c];
+      if (
+        targetPiece === "" ||
+        targetPiece.includes(piece.includes("/w") ? "/b" : "/w")
+      ) {
+        moves[r][c] = true;
+      }
+    }
+  }
+
+  return moves;
+}
+
+function checkGameStatus() {
+  const whiteKingExists = document.querySelector("img[src='./icons/wk.png']");
+  const blackKingExists = document.querySelector("img[src='./icons/bk.png']");
+  if (!whiteKingExists) {
+    alert("Black wins!");
+  } else if (!blackKingExists) {
+    alert("White wins!");
+  }
 }
 
 createBoard();

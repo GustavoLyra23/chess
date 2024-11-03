5d1ce622299fb112e7e2113fb023772ed39e6a2d
GustavoLyra23 <gustavolyra23@gmail.com>
2024-11-03 03:19:37 -0300

    refactor app.js

--- app.js
+++ app.js
@@ -1,14 +1,50 @@
 const board = document.querySelector(".chessboard");
 
 const initialBoard = [
-  ["♜", "♞", "♝", "♛", "♚", "♝", "♞", "♜"],
-  ["♟", "♟", "♟", "♟", "♟", "♟", "♟", "♟"],
+  [
+    "./icons/br.png",
+    "./icons/bn.png",
+    "./icons/bb.png",
+    "./icons/bq.png",
+    "./icons/bk.png",
+    "./icons/bb.png",
+    "./icons/bn.png",
+    "./icons/br.png",
+  ],
+  [
+    "./icons/bp.png",
+    "./icons/bp.png",
+    "./icons/bp.png",
+    "./icons/bp.png",
+    "./icons/bp.png",
+    "./icons/bp.png",
+    "./icons/bp.png",
+    "./icons/bp.png",
+  ],
   ["", "", "", "", "", "", "", ""],
   ["", "", "", "", "", "", "", ""],
   ["", "", "", "", "", "", "", ""],
   ["", "", "", "", "", "", "", ""],
-  ["♙", "♙", "♙", "♙", "♙", "♙", "♙", "♙"],
-  ["♖", "♘", "♗", "♕", "♔", "♗", "♘", "♖"],
+  [
+    "./icons/wp.png",
+    "./icons/wp.png",
+    "./icons/wp.png",
+    "./icons/wp.png",
+    "./icons/wp.png",
+    "./icons/wp.png",
+    "./icons/wp.png",
+    "./icons/wp.png",
+  ],
+  [
+    "./icons/wr.png",
+    "./icons/wn.png",
+    "./icons/wb.png",
+    "./icons/wq.png",
+    "./icons/wk.png",
+    "./icons/wb.png",
+    "./icons/wn.png",
+    "./icons/wr.png",
+  ],
 ];
 
 const copyBoard = initialBoard.map((row) => [...row]);
@@ -30,6 +66,8 @@ function createBoard() {
 function createSquare(row, col) {
   const square = document.createElement("div");
   square.classList.add("square");
+  square.dataset.row = row;
+  square.dataset.col = col;
   if ((row + col) % 2 === 0) {
     square.classList.add("light");
   } else {
@@ -39,36 +77,59 @@ function createSquare(row, col) {
 }
 
 function createPieceElement(piece, row, col) {
-  const pieceElement = document.createElement("span");
-  pieceElement.textContent = piece;
-  pieceElement.classList.add("chess-piece", `${row}${col}`);
-  if (piece.charCodeAt(0) >= 9812 && piece.charCodeAt(0) <= 9817) {
-    pieceElement.classList.add("white-piece");
-  } else {
-    pieceElement.classList.add("black-piece");
-  }
+  const pieceElement = document.createElement("img");
+  pieceElement.src = piece;
+  pieceElement.classList.add("chess-piece");
+  pieceElement.dataset.row = row;
+  pieceElement.dataset.col = col;
   return pieceElement;
 }
 
 document.addEventListener("keypress", () => {
   document.querySelector("h2").innerText = "Game started!";
   document.querySelectorAll(".chess-piece").forEach((piece) => {
-    piece.addEventListener("click", (e) => {
-      clearHighlights();
-      const moves = checkPieceToMove(e.target);
-      highlightMoves(moves);
-      console.log(moves);
-      console.log(e.target.classList);
-      console.log("You clicked on a chess piece!");
-    });
+    piece.addEventListener("click", pieceClickHandler);
   });
 });
+
+function pieceClickHandler(e) {
+  clearHighlights();
+  const moves = checkPieceToMove(e.target);
+  highlightMoves(moves);
+  console.log(moves);
+  console.log(e.target.classList);
+  console.log("You clicked on a chess piece!");
+
+  document.querySelectorAll(".highlight").forEach((square) => {
+    square.addEventListener("click", squareClickHandler);
+  });
+}
+
+function squareClickHandler(e) {
+  const piece = document.querySelector(".chess-piece[data-selected='true']");
+  if (piece) {
+    e.target.appendChild(piece);
+    const row = parseInt(piece.dataset.row);
+    const col = parseInt(piece.dataset.col);
+    const newRow = parseInt(e.target.dataset.row);
+    const newCol = parseInt(e.target.dataset.col);
+    copyBoard[newRow][newCol] = copyBoard[row][col];
+    copyBoard[row][col] = "";
+    piece.dataset.row = newRow;
+    piece.dataset.col = newCol;
+    piece.removeAttribute("data-selected");
+    clearHighlights();
+    document.querySelectorAll(".highlight").forEach((square) => {
+      square.removeEventListener("click", squareClickHandler);
+    });
+  }
+}
 
 function calculatePawnMoves(row, column) {
   const moves = initializeMovesMatrix();
   const piece = copyBoard[row][column];
 
-  if (piece === "♟") {
+  if (piece === "./icons/bp.png") {
     if (row + 1 < 8 && copyBoard[row + 1][column] === "") {
       moves[row + 1][column] = true;
       if (row === 1 && copyBoard[row + 2][column] === "") {
@@ -78,20 +139,18 @@ function calculatePawnMoves(row, column) {
     if (
       row + 1 < 8 &&
       column + 1 < 8 &&
-      copyBoard[row + 1][column + 1].charCodeAt(0) >= 9812 &&
-      copyBoard[row + 1][column + 1].charCodeAt(0) <= 9817
+      copyBoard[row + 1][column + 1].startsWith("./icons/w")
     ) {
       moves[row + 1][column + 1] = true;
     }
     if (
       row + 1 < 8 &&
       column - 1 >= 0 &&
-      copyBoard[row + 1][column - 1].charCodeAt(0) >= 9812 &&
-      copyBoard[row + 1][column - 1].charCodeAt(0) <= 9817
+      copyBoard[row + 1][column - 1].startsWith("./icons/w")
     ) {
       moves[row + 1][column - 1] = true;
     }
-  } else if (piece === "♙") {
+  } else if (piece === "./icons/wp.png") {
     if (row - 1 >= 0 && copyBoard[row - 1][column] === "") {
       moves[row - 1][column] = true;
       if (row === 6 && copyBoard[row - 2][column] === "") {
@@ -101,16 +160,14 @@ function calculatePawnMoves(row, column) {
     if (
       row - 1 >= 0 &&
       column + 1 < 8 &&
-      copyBoard[row - 1][column + 1].charCodeAt(0) >= 9818 &&
-      copyBoard[row - 1][column + 1].charCodeAt(0) <= 9823
+      copyBoard[row - 1][column + 1].startsWith("./icons/b")
     ) {
       moves[row - 1][column + 1] = true;
     }
     if (
       row - 1 >= 0 &&
       column - 1 >= 0 &&
-      copyBoard[row - 1][column - 1].charCodeAt(0) >= 9818 &&
-      copyBoard[row - 1][column - 1].charCodeAt(0) <= 9823
+      copyBoard[row - 1][column - 1].startsWith("./icons/b")
     ) {
       moves[row - 1][column - 1] = true;
     }
@@ -124,13 +181,14 @@ function initializeMovesMatrix() {
 }
 
 function checkPieceToMove(piece) {
-  const row = parseInt(piece.classList[1][0]);
-  const column = parseInt(piece.classList[1][1]);
-  const pieceText = piece.innerText;
-  console.log(pieceText);
-  switch (pieceText) {
-    case "♟":
-    case "♙":
+  const row = parseInt(piece.dataset.row);
+  const column = parseInt(piece.dataset.col);
+  const pieceSrc = piece.src.split("/").pop();
+  console.log(pieceSrc);
+  switch (pieceSrc) {
+    case "bp.png":
+    case "wp.png":
+      piece.setAttribute("data-selected", "true");
       return calculatePawnMoves(row, column);
     default:
       return initializeMovesMatrix();
@@ -142,7 +200,7 @@ function highlightMoves(moves) {
     for (let j = 0; j < 8; j++) {
       if (moves[i][j]) {
         const square = document.querySelector(
-          `.square:nth-child(${i * 8 + j + 1})`
+          `.square[data-row='${i}'][data-col='${j}']`
         );
         square.classList.add("highlight");
       }
